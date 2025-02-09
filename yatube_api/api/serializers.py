d76eccfe2fd5364f6a5ed078f097c5ba4a2e5d5c
Max Zhikin <maxzhikin@gmail.com>
2025-02-09 23:40:31 +0700

    5

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,13 +1,16 @@
 from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField as SRField
 from posts.models import Comment, Post, User, Follow, Group
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(slug_field='username',
-                                          read_only=True)
+    author = SRField(
+        slug_field='username',
+        read_only=True)
     image = serializers.ImageField(required=False, allow_null=True)
-    group = serializers.PrimaryKeyRelatedField(queryset=Group.objects.all(),
-                                               required=False)
+    group = serializers.PrimaryKeyRelatedField(
+        queryset=Group.objects.all(),
+        required=False)
 
     class Meta:
         fields = '__all__'
@@ -15,8 +18,9 @@ class PostSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(read_only=True,
-                                          slug_field='username')
+    author = SRField(
+        read_only=True,
+        slug_field='username')
     post = serializers.PrimaryKeyRelatedField(read_only=True)
     created = serializers.DateTimeField(read_only=True)
 
@@ -26,7 +30,7 @@ class CommentSerializer(serializers.ModelSerializer):
 
 
 class FollowSerializer(serializers.ModelSerializer):
-    user = serializers.SlugRelatedField(
+    user = SRField(
         slug_field='username',
         read_only=True,
         default=serializers.CurrentUserDefault()
